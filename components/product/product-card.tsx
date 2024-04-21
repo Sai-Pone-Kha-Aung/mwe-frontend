238265592e8892d30088c7eedfb75fcf3ec256b1
Sai Pone Kha Aung <saiponekhaaaung@gmail.com>
2024-04-22 00:50:39 +0700

    Add the product preview

--- components/product/product-card.tsx
+++ components/product/product-card.tsx
@@ -6,14 +6,26 @@ import IconButton from "@/components/ui/icon-button";
 import { Expand, ShoppingCart } from "lucide-react";
 import  Currency  from "@/components/ui/currency";
 import { useRouter } from "next/navigation";
+import { MouseEventHandler } from "react";
+import usePreviewModal from "@/hooks/use-preview-modal";
 
 interface ProductCardProps {
     data: Product
 }
 const ProductCard: React.FC<ProductCardProps> = ({data}) => {
-    const imageUrl = data?.images?.[0]?.url;
-    const isValidUrl = imageUrl && (imageUrl.startsWith('http://') || imageUrl.startsWith('https://'));
+    
     const router = useRouter();
+    const previewModal = usePreviewModal();
+
+    const imageUrl = data?.images?.[0]?.url;
+    const isValidUrl = imageUrl && (imageUrl.startsWith('http://') || imageUrl.startsWith
+    ('https://'));
+
+    const onPreview: MouseEventHandler<HTMLButtonElement> = (e) => {
+        e.stopPropagation();
+        previewModal.onOpen(data);
+    }
+
     const handleClick = () => {
         router.push(`/product/${data?.id}`);
     }
@@ -30,7 +42,7 @@ const ProductCard: React.FC<ProductCardProps> = ({data}) => {
             <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
                 <div className="flex gap-x-6 justify-center">
                     <IconButton 
-                    onClick={() => {}} 
+                    onClick={onPreview} 
                     icon={<Expand size={20} className="text-gray-600"/>} 
                     />
                     <IconButton 
