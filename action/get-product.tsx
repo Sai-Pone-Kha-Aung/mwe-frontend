3e11aba65fc6b4f1f528087996bf434a899babd4
Sai Pone Kha Aung <saiponekhaaaung@gmail.com>
2024-04-21 16:07:04 +0700

    Add the product detail page

---
+++ action/get-product.tsx
@@ -0,0 +1,15 @@
+import  { Product } from '@/types';
+
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`;
+
+const getProduct = async (id: string): Promise<Product> => {
+    const res = await fetch(`${URL}/${id}`);
+
+    if(!res.ok) {
+        throw new Error("Failed to fetch categories");
+    }
+    
+    return res.json();
+}
+
+export default getProduct;
